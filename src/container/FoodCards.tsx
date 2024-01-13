eddc7c01dbb7401657aa6768a747767827969734
Bekbolot Nomanov <nomanovbekbolot7@gmail.com>
2024-01-13 13:01:47 +0600

    initials commit

---
+++ src/container/FoodCards.tsx
@@ -0,0 +1,12 @@
+
+
+const Foods: Food[] = [
+  {id:1, name: 'Hamburger', price: 80, img: '' },
+  {id:2, name: 'Cheeseburger', price: 90, img: '' },
+  {id:3, name: 'Fries', price: 45, img: '' },
+  {id:4, name: 'Coffee', price: 70, img: '' },
+  {id:5, name: 'Tea', price: 50, img: '' },
+  {id:6, name: 'Cola', price: 40, img: '' },
+];
+
+export default Foods
---
+++ src/type.d.ts
@@ -0,0 +1,6 @@
+interface Food  {
+  id: number;
+  name: string;
+  price: number;
+  img: string;
+}
